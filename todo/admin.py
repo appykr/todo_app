f80fca58ee631dd40ae18b7b976e6d3960058722
Arpit Kumar <arpitk2806@gmail.com>
2024-08-21 00:54:02 +0530

    Add Admin customization

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from . models import Task
 
 # Register your models here.
-class TaskAdmin(admin.ModelAdmin):
+class TaskAdmin(ModelAdmin):
     list_display=('task', 'is_completed', 'updated_at')
     search_fields = ('task',)
 admin.site.register(Task, TaskAdmin)
